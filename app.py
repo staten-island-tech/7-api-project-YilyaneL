9bbe75e1ab55e18431f850fd8df08c632fc5127c
BathTubTheGreat <yilyanel@nycstudents.net>
2026-04-22 10:13:11 -0400

    jmkxdryrysjhaerzhaergRAW

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
 import requests
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
 
-def getIP(ip,accesskey):
+root = tk.Tk()
+root.title("I SPY WITH MY LITTLE EYE AN IP")
+root.geometry("700x600")
+style = ttk.Style()
+style.theme_use("clam")
+accesskey = "88f4a26ebfa2c094b719135c5535b0b4"
+
+instruct = ttk.Label(root, text="IP you want to search:", font=("Arial", 15))
+instruct.place(x=0,y=0)
+
+ipinput = ttk.Entry(root, font=("Arial", 16), width=14)
+ipinput.place(x=205,y=0)
+
+copyright = ttk.Label(root, text="YILYANE LOUNAS ©", font=("Arial",11))
+copyright.place(x=550,y=0)
+
+def getIP():
+
+    ip = ipinput.get()
+
     response = requests.get(f"http://api.ipstack.com/{ip}?access_key={accesskey}")
     if response.status_code != 200:
-        print("nothing is coming check your internet")
+        messagebox.showerror("Error", "Error check your connection")
 
     ipdata = response.json()
     
@@ -19,8 +41,9 @@ def getIP(ip,accesskey):
     current_time = ipdata["time_zone"]["current_time"]
     currency = ipdata["currency"]["code"]
     isp = ipdata["connection"]["isp"]
+    print(zip)
 
-    print(region)
+enterbutton = ttk.Button(root, text="Enter", command=getIP)
+enterbutton.place(x=380,y=0)
 
-getIP("165.155.162.14","88f4a26ebfa2c094b719135c5535b0b4")
-
+root.mainloop()
